8b61102e3af335b8621887ec9d585528dc165e5b
igor725 <admin@igvx.ru>
2019-06-08 17:35:44 +0300

    whoops

--- main.lua
+++ main.lua
@@ -53,17 +53,6 @@ function onPlayerDestroy(player)
 	end
 end
 
-function postPlayerPlaceBlock(player, x, y, z, id)
-	local world = getWorld(player)
-	for dx = -1, 1, 1 do -- Update neighboring blocks
-		for dy = -1, 1, 1 do
-			for dz = -1, 1, 1 do
-				world:updateWaterBlock(x - dx, y - dy, z - dz)
-			end
-		end
-	end
-end
-
 function onPlayerChatMessage(player, message)
 	local starts = message:sub(1, 1)
 	if not message:startsWith('#', '>', '/')then
