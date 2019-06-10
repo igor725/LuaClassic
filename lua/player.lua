a3434a672548fe0e3e1a4e2267439e4a7a005f97
scaledteam <scaled@scaledteam.ru>
2019-06-11 02:35:14 +0500

    fix fall damage while tp

--- lua/player.lua
+++ lua/player.lua
@@ -234,12 +234,13 @@ local player_mt = {
 		local pos = self.pos
 		if not self.isSpawned then
 			pos.x, pos.y, pos.z = x, y, z
-			self.landed = true
+			self.oldDY, self.oldSpeedY2 = 0, 0
 			return
 		
 		elseif self.isTeleported then
 			self.isTeleported = false
-			self.landed = true
+			pos.x, pos.y, pos.z = x, y, z
+			self.oldDY, self.oldSpeedY2 = 0, 0
 			return
 		elseif pos.x ~= x or pos.y ~= y or pos.z ~= z then
 			local dx, dy, dz = x - pos.x, y - pos.y, z - pos.z
