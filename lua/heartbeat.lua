1c44ed3bf7ce8136781b35c510758a3578967208
igor725 <admin@igvx.ru>
2019-06-12 20:54:14 +0300

    heartbeat.lua and mob.lua cleanup

--- lua/heartbeat.lua
+++ lua/heartbeat.lua
@@ -33,7 +33,7 @@ hooks:add('onInitDone', 'heartbeat', function()
 		_HEARTBEAT_DELAY = 50
 		_HEARTBEAT_PORT = 80
 		_HEARTBEAT_VALID = '^http://www%.classicube%.net/server/play/'
-		_HEARTBEAT_URL = '/server/heartbeat?name=%s&port=%d&users=%d&max=%d&salt=%s&public=%s&software=LuaClassic&web=%s'
+		_HEARTBEAT_URL = '/server/heartbeat?name=%s&port=%d&users=%d&max=%d&salt=%s&public=%s&software=%s&web=%s'
 		_HEARTBEAT_CLK = function()
 			local ip = gethostbyname(_HEARTBEAT_HOST)
 			local fd, err = connectSock(ip, _HEARTBEAT_PORT)
@@ -46,9 +46,10 @@ hooks:add('onInitDone', 'heartbeat', function()
 			local sOnline = getCurrentOnline()
 			local sPublic = config:get('heartbeatPublic')
 			local sMax = config:get('maxPlayers')
+			local sSoftware = cpe.softwareName
 			local sWeb = config:get('acceptWebsocket')
 
-			local request = (_HEARTBEAT_URL):format(sName, sPort, sOnline, sMax, sSalt, sPublic, sWeb)
+			local request = (_HEARTBEAT_URL):format(sName, sPort, sOnline, sMax, sSalt, sPublic, sSoftware sWeb)
 			sendMesg(fd, ('GET %s HTTP/1.1\n'):format(request))
 			sendMesg(fd, ('Connection: close\n'):format(_HEARTBEAT_HOST))
 			sendMesg(fd, ('Accept: */*\n'):format(_HEARTBEAT_HOST))
@@ -56,12 +57,12 @@ hooks:add('onInitDone', 'heartbeat', function()
 			sendMesg(fd, ('Host: www.%s\n\n'):format(_HEARTBEAT_HOST))
 
 			local resp = receiveLine(fd)
-			local requestOk = false
-			local respHdrs = {}
-			if resp:lower():find('^http/.+200 ok$')then
-				requestOk = true
+			if not resp:lower():find('^http/.+200 ok$')then
+				lgo.error('Heartbeat server responded', resp)
+				return
 			end
 
+			local respHdrs = {}
 			while true do
 				local line = receiveLine(fd)
 				if line == ''then break end
@@ -71,27 +72,16 @@ hooks:add('onInitDone', 'heartbeat', function()
 				end
 			end
 
-			local len = respHdrs['content-length']
-
-			if type(len) == 'number'then
-				local respBody = receiveString(fd, len)
-				if respBody:find(_HEARTBEAT_VALID)then
-					_HEARTBEAT_PLAY = respBody
-				else
-					log.error('Heartbeat: ' .. respBody)
-				end
-			else
-				while true do
-					local line = receiveLine(fd)
-					if not line or line == ''then break end
-					if line:find(_HEARTBEAT_VALID)then
-						_HEARTBEAT_SALT = sSalt
-						if _HEARTBEAT_PLAY ~= line then
-							_HEARTBEAT_PLAY = line
-							log.info('Server URL: ' .. line)
-						end
-						break
+			while true do
+				local line = receiveLine(fd)
+				if not line or line == ''then break end
+				if line:find(_HEARTBEAT_VALID)then
+					_HEARTBEAT_SALT = sSalt
+					if _HEARTBEAT_PLAY ~= line then
+						_HEARTBEAT_PLAY = line
+						log.info('Server URL:', line)
 					end
+					break
 				end
 			end
 			closeSock(fd)
