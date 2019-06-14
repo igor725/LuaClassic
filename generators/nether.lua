5de490404d053c512bc6f073a599c02f0a0b9faa
scaledteam <scaled@scaledteam.ru>
2019-06-15 04:24:57 +0500

    Update nether generator

--- generators/nether.lua
+++ generators/nether.lua
@@ -5,6 +5,8 @@
 
 local GEN_BIOME_STEP = 20
 local GEN_BIOME_RADIUS = 2
+local GEN_ENABLE_HOUSES = true
+local GEN_HOUSES_COUNT_MULT = 1 / 50000
 
 local heightGrass
 local heightLava
@@ -92,30 +94,31 @@ local function layersGenerate(dx, dy, dz)
 		for x = 0, biomesSizeX do
 			layers[layer][x] = {}
 			for z = 0, biomesSizeZ do
-				layers[layer][x][z] = 1
+				local height = math.random(-dy / LAYERS_COUNT / 2, dy / LAYERS_COUNT / 2)
+				layers[layer][x][z] = height
 			end
 		end
 
-		local radius = 3
+		--[[local radius = 3
 		local BIOME_COUNT = dx * dz / GEN_BIOME_STEP / radius / 128 + 1
 		local radius2 = radius * radius
 
 		for i = 1, BIOME_COUNT do
 			local x = math.random(biomesSizeX)
 			local z = math.random(biomesSizeZ)
-			local biome = math.random(0, 5)
+			local height = math.random(0, 1)
 
 			for dx = -radius, radius do
 				for dz = -radius, radius do
 					if
-					dx*dx + dz*dz < radius2
-					and layers[layer][x + dx] ~= nil and biomes[x + dx][z + dz] ~= nil
+						dx*dx + dz*dz < radius2
+						and layers[layer][x + dx] ~= nil and layers[layer][x + dx][z + dz] ~= nil
 					then
-						layers[layer][x + dx][z + dz] = biome
+						layers[layer][x + dx][z + dz] = height
 					end
 				end
 			end
-		end
+		end]]--
 	end
 end
 
@@ -126,6 +129,12 @@ local function getLayerMultiplier(layer, x, z)
 
 	return (layers[layer][hx][hz  ] * (1 - percentX) + layers[layer][hx + 1][hz  ] * percentX) * (1 - percentZ)
 		+ (layers[layer][hx][hz + 1] * (1 - percentX) + layers[layer][hx + 1][hz + 1] * percentX) * percentZ
+
+	--[[return math.floor(
+		  (layers[layer][hx][hz  ] * (1 - percentX) + layers[layer][hx + 1][hz  ] * percentX) * (1 - percentZ)
+		+ (layers[layer][hx][hz + 1] * (1 - percentX) + layers[layer][hx + 1][hz + 1] * percentX) * percentZ
+		+ 0.5
+	)]]
 end
 
 -- Generate
@@ -239,14 +248,105 @@ local function threadTerrain(mapaddr, dx, dy, dz, heightMap, heightLava, startX,
 			-- temp for layers
 			for layer = 1, #layers do
 				local multiplier = getLayerMultiplier(layer, x, z)
-				if multiplier > 0 then
-					local layerHeight = dy * layer / (#layers + 1)
-					local height2 = (height1 - heightGrass) / 2 * multiplier
-					for y = math.floor(layerHeight - height2 + 1), layerHeight + height2 do
-						if 1 < y and y < dy - 1 then
-							map[offset + y * step] = 45
-						end
+				
+				local layerHeight = dy * layer / (#layers + 1)
+				for y = math.floor(layerHeight - multiplier / 2) + math.random(0, 1), layerHeight + multiplier - 1 do
+					if 0 < y and y < dy - 1 then
+						map[offset + y * step] = 45
 					end
+				end
+			end
+		end
+	end
+end
+
+local function generateHouse(mapaddr, dimx, dimy, dimz, seed)
+	set_debug_threadname('HousesGenerator')
+	math.randomseed(seed)
+
+	local map = ffi.cast('char*', mapaddr)
+	local size = dimx * dimy * dimz + 4
+
+	local SetBlock = function(x, y, z, id)
+		map[(y * dimz + z) * dimx + x + 4] = id
+	end
+
+	local GetBlock = function(x, y, z)
+		return map[(y * dimz + z) * dimx + x + 4]
+	end
+
+	local HOUSE_COUNT = math.ceil(dimx * dimz * GEN_HOUSES_COUNT_MULT)
+	local materials = {49, 44, 43}
+
+	for i = 1, HOUSE_COUNT do
+		local startX = math.random(4, dimx - 11)
+		local startZ = math.random(4, dimz - 9)
+		local endX = startX + math.random(6, 8)
+		local endZ = startZ + math.random(4, 6)
+
+		-- Find max height
+		local cancel = false
+
+		local maxHeight = 0
+		local minHeight = dimy
+		local tempHeight
+		for x = startX, endX do
+			for z = startZ, endZ do
+				tempHeight = getHeight(x, z)
+				if tempHeight > maxHeight then
+					maxHeight = tempHeight
+				end
+				if tempHeight < minHeight then
+					minHeight = tempHeight
+				end
+				if tempHeight < heightLava or tempHeight > dimy - 11 then
+					cancel = true
+					break
+				end
+			end
+		end
+
+		if not cancel then
+			maxHeight = maxHeight + 1
+
+			local lengthX = endX - startX + 1
+			for z = startZ, endZ do
+				for y = minHeight, maxHeight do
+					ffi.fill(map + (y * dimz + z) * dimx + startX + 4, lengthX, materials[1])
+				end
+			end
+
+
+			-- walls
+			for i = 1, #materials do
+				ffi.fill(map + ((maxHeight + i) * dimz + startZ) * dimx + startX + 4, lengthX, materials[i])
+				ffi.fill(map + ((maxHeight + i) * dimz + endZ) * dimx + startX + 4, lengthX, materials[i])
+
+				for z = startZ + 1, endZ - 1 do
+					SetBlock(startX, maxHeight + i, z, materials[i])
+					SetBlock(endX, maxHeight + i, z, materials[i])
+				end
+
+				SetBlock(startX + 2, maxHeight + i, startZ, 0)
+			end
+
+			-- SetBlock(startX + 2, maxHeight + i, startZ, 0)
+
+			local j = 1
+			while GetBlock(startX + 2, maxHeight - j, startZ - j) == 0 do
+				SetBlock(startX + 2, maxHeight - j, startZ - j, materials[1])
+				j = j + 1
+			end
+
+			maxHeight = maxHeight + 4
+
+			for i = -1, math.ceil(math.min(endX - startX - 1, endZ - startZ - 1) / 2) do
+				ffi.fill(map + ((maxHeight + i) * dimz + startZ + i) * dimx + startX + i + 4, lengthX - 2 * i, materials[3])
+				ffi.fill(map + ((maxHeight + i) * dimz + endZ - i) * dimx + startX + i + 4, lengthX - 2 * i, materials[3])
+
+				for z = startZ + i + 1, endZ - i - 1 do
+					SetBlock(startX + i, maxHeight + i, z, materials[3])
+					SetBlock(endX - i, maxHeight + i, z, materials[3])
 				end
 			end
 		end
@@ -281,6 +381,12 @@ return function(world, seed)
 
 		local terrain_gen = lanes.gen('math,ffi', threadTerrain)
 		table.insert(threads, terrain_gen(mapaddr, dx, dy, dz, heightMap, heightLava, startX, endX, layers))
+	end
+
+	if GEN_ENABLE_HOUSES then
+		local houses_gen = lanes.gen('math,ffi', generateHouse)
+		table.insert(threads, houses_gen(mapaddr, dx, dy, dz, seed))
+		log.debug('HousesGenerator: started')
 	end
 
 	watchThreads(threads)
