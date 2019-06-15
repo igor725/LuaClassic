fbb1d8026685569be034b2a79405fa5a409cfe15
scaledteam <scaled@scaledteam.ru>
2019-06-15 15:32:34 +0500

    Add another item to item test

--- gamemodes/survival/items.lua
+++ gamemodes/survival/items.lua
@@ -1,13 +1,24 @@
-
+-- #gmLoad('items')
 
 -- test	
 local opts = {
 	id = 41,
-	name = 'Golden pickaxe',
+	name = 'Iron pickaxe',
 	minX = 8,
 	maxX = 8,
 	leftTex = 16*6,
 	rightTex = 16*6,
+	blockDraw = BD_TRANSPARENT
+}
+BlockDefinitions:createEx(opts)
+
+opts = {
+	id = 42,
+	name = 'Iron sword',
+	minX = 8,
+	maxX = 8,
+	leftTex = 16*6+1,
+	rightTex = 16*6+1,
 	blockDraw = BD_TRANSPARENT
 }
 BlockDefinitions:createEx(opts)
