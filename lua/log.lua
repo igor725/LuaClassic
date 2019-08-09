1b751655960c8bdd0c38c75ec04de09f56c91a9c
igor725 <admin@igvx.ru>
2019-08-09 10:24:59 +0300

    "type" and "color" tables from log.lua are merged

--- lua/log.lua
+++ lua/log.lua
@@ -9,44 +9,34 @@ LOG_CHAT  = 3
 LOG_INFO  = 1
 LOG_ERROR = 0
 
-local types = {
-	[LOG_DEBUG] = 'DEBUG',
-	[LOG_WARN]  = 'WARN ',
-	[LOG_CHAT]  = 'CHAT ',
-	[LOG_INFO]  = 'INFO ',
-	[LOG_ERROR] = 'ERROR'
-}
-
 log = {
-	colors = {
-		[LOG_DEBUG] = '1;34',
-		[LOG_WARN]  = '35',
-		[LOG_CHAT]  = '1;33',
-		[LOG_INFO]  = '1;32',
-		[LOG_ERROR] = '1;31'
+	types = {
+		[LOG_DEBUG] = {'DEBUG', '1;34'},
+		[LOG_WARN ] = {'WARN ', '35'  },
+		[LOG_CHAT ] = {'CHAT ', '1;33'},
+		[LOG_INFO ] = {'INFO ', '1;32'},
+		[LOG_ERROR] = {'ERROR', '1;31'}
 	},
 	level = tonumber(os.getenv('LOGLEVEL'))or LOG_WARN
 }
 
-if not enableConsoleColors then
-	log.colors = nil
-end
-
 local function printlogline(ltype, ...)
 	if log.level < ltype then return end
-	local color = (log.colors and log.colors[ltype])or nil
+	local ltdata = log.types[ltype]
+	local color = enableConsoleColors and ltdata[2]
 	local fmt
 	local time, mtime = math.modf(gettime())
 	mtime = mtime * 999
 
 	if color then
 		fmt = os.date('%H:%M:%S.%%03d [\27[%%sm%%s\27[0m] ', time)
-		fmt = (fmt):format(mtime, color, types[ltype])
+		fmt = (fmt):format(mtime, color, ltdata[1])
 	else
 		fmt = os.date('%H:%M:%S.%%03d [%%s] ', time)
-		fmt = (fmt):format(mtime, types[ltype])
+		fmt = (fmt):format(mtime, ltdata[1])
 	end
 	io.write(fmt)
+
 	local idx = 1
 	while true do
 		local val = select(idx, ...)
